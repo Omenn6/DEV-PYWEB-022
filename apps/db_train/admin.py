a90feb4046fda89316510374d59857feee428e0c
Pavel <bruchkovskiiy@mail.ru>
2026-03-31 19:50:35 +0300

    Часть 3.4

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Зарегистрируйте свои модели в админ панели здесь
+from django.contrib import admin
+from .models import Author
+
+admin.site.register(Author)
+
+def __str__(self):
+    initials = None  # Инициалы
+    if self.first_name and self.middle_name:
+        initials = f"{self.first_name.upper()[0]}.{self.middle_name.upper()[0]}."
+    return f"{self.username} - {self.last_name} {initials}"
